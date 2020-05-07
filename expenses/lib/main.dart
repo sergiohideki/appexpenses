304648394eb0b1d32a07c4f6eb563077764fdd02
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-07 20:39:47 -0300

    Inserindo textfield e estilizando

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -35,7 +35,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -90,6 +89,34 @@ class MyHomePage extends StatelessWidget {
                 ),
               );
             }).toList(), // convertendo o objeto para uma lista
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Título'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Valor (R\$)',
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      FlatButton(
+                        child: Text('Nova Transação'),
+                        textColor: Colors.purple,
+                        onPressed: () {},
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
