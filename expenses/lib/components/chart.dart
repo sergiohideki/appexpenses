8b35dc804edc25f4de9856992b50d35fd93853e0
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-08 17:29:07 -0300

    Chart Widget e Chart Bar

--- expenses/lib/components/chart.dart
+++ expenses/lib/components/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
+import '../components/chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransaction;
@@ -11,20 +12,17 @@ class Chart extends StatelessWidget {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
 
-    double totalSum = 0.0;
+      double totalSum = 0.0;
 
-    for(var i = 0; i < recentTransaction.length; i++){
-      bool sameDay = recentTransaction[i].date.day == weekDay.day;
-      bool sameMonth = recentTransaction[i].date.month == weekDay.month;
-      bool sameYear = recentTransaction[i].date.year == weekDay.year;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool sameYear = recentTransaction[i].date.year == weekDay.year;
 
-      if(sameDay && sameMonth && sameYear){
-        totalSum += recentTransaction[i].value;
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransaction[i].value;
+        }
       }
-    }
-
-  print(DateFormat.E().format(weekDay)[0]);
-  print(totalSum);
 
       return {
         'day': DateFormat.E().format(weekDay)[0],
@@ -35,12 +33,17 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    groupedTransactions;
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactions.map((tr) {
+          return ChartBar(
+            label: tr['day'],
+            value: tr['value'],
+            percentage: 0.3
+          );
+        }).toList(),
       ),
     );
   }
